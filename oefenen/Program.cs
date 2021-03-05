43ec10bbe23c76678587e95984743ea700030778
Arthur Stragier <71759234+ArthurStragier5@users.noreply.github.com>
2021-03-05 13:14:09 +0100

    Add project files.

---
+++ oefenen/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace oefenen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
